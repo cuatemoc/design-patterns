a90f13c5ca05f067f5fa0aa5ced72695ea500923
SLem <slemjet@gmail.com>
2018-08-29 22:23:44 +0200

    initial commit for Builder

---
+++ visitor/src/test/java/visitors/acyclic/ExpressionPrinterTest.java
@@ -0,0 +1,23 @@
+package visitors.acyclic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExpressionPrinterTest {
+
+    @Test
+    public void test() {
+        // given
+        AdditionExpression expression = new AdditionExpression(new DoubleExpression(1d),
+                new AdditionExpression(new DoubleExpression(2d), new DoubleExpression(3d)));
+
+        ExpressionPrinter expressionPrinter = new ExpressionPrinter();
+
+        // when
+        expressionPrinter.visit(expression);
+
+        //then
+        assertEquals("(1.0+(2.0+3.0))", expressionPrinter.toString());
+    }
+}
---
+++ visitor/src/test/java/visitors/doubledispatch/ExpressionCalculatorTest.java
@@ -0,0 +1,22 @@
+package visitors.doubledispatch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExpressionCalculatorTest {
+    @Test
+    public void test() {
+        // given
+        AdditionExpression expression = new AdditionExpression(new DoubleExpression(1d),
+                new AdditionExpression(new DoubleExpression(2d), new DoubleExpression(3d)));
+
+        ExpressionCalculator expressionCalculator = new ExpressionCalculator();
+
+        // when
+        expressionCalculator.visit(expression);
+
+        //then
+        assertEquals(6d, expressionCalculator.getValue());
+    }
+}
