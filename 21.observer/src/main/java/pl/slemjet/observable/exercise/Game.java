e735288ccc7e3e31533613ecc6894b7ba164ac28
SLem <slemjet@gmail.com>
2019-09-01 15:41:46 +0200

    Observable - exercise

---
+++ 21.observer/src/main/java/pl/slemjet/observable/exercise/Game.java
@@ -0,0 +1,62 @@
+package pl.slemjet.observable.exercise;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+
+class Game {
+    public Event<Void> ratEnters = new Event<>();
+    public Event<Void> ratDies = new Event<>();
+    public Event<Rat> notifyRat = new Event<>();
+}
+
+class Event<T> {
+    private List<BiConsumer<Object, T>> consumers = new ArrayList<>();
+
+    public void subscribe(BiConsumer<Object, T> consumer) {
+        consumers.add(consumer);
+    }
+
+    public void invoke(Object sender, T arg) {
+        for (BiConsumer<Object, T> consumer : consumers)
+            consumer.accept(sender, arg);
+    }
+}
+
+class Rat implements Closeable {
+    private Game game;
+    public int attack = 1;
+
+    public Rat(Game game) {
+        // todo: rat enters game here
+        this.game = game;
+
+        // rat enters action
+        game.ratEnters.subscribe((sender, value) -> {
+            if (sender != this) {
+                attack++;
+                game.notifyRat.invoke(this, (Rat) sender);
+            }
+        });
+
+        // notify rat action
+        game.notifyRat.subscribe((o, rat) -> {
+            if (rat == this)
+                ++attack;
+        });
+
+        // rat leaves action
+        game.ratDies.subscribe((sender, value) -> --attack);
+
+        game.ratEnters.invoke(this, null);
+    }
+
+    @Override
+    public void close() throws IOException {
+        // todo: rat dies ;(
+        game.ratDies.invoke(this, null);
+    }
+}
